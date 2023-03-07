687d77767a4ac819732c237da5c1f8e9b3419ee9
Santiago Armstrong <santiago.armstrong@cencosud.cl>
2023-03-07 12:23:33 -0300

    added simple test

--- tests/samples/note1.py
+++ tests/samples/note1.py
@@ -6,7 +6,7 @@
 
 # main imports
 from local_databricks.ldbutils import notebook
-# from local_databricks.session import SPARK
+from local_databricks.session import SPARK
 
 # COMMAND ----------
 
@@ -34,4 +34,31 @@ notebook.run_notebook(path = "tests/samples/note2.py", timeout_seconds = 60, arg
 
 # read databricks sql table
 # table_name = "some_table"
-# notebook.display(SPARK.sql(f"SELECT * FROM {table_name}"))
+emp = [(1,"Smith",-1,"2018","10","M",3000), \
+    (2,"Rose",1,"2010","20","M",4000), \
+    (3,"Williams",1,"2010","10","M",1000), \
+    (4,"Jones",2,"2005","10","F",2000), \
+    (5,"Brown",2,"2010","40","",-1), \
+      (6,"Brown",2,"2010","50","",-1) \
+  ]
+empColumns = ["emp_id","name","superior_emp_id","year_joined", \
+       "emp_dept_id","gender","salary"]
+
+empDF = SPARK.createDataFrame(data=emp, schema = empColumns)
+empDF.printSchema()
+empDF.show(truncate=False)
+
+dept = [("Finance",10), \
+    ("Marketing",20), \
+    ("Sales",30), \
+    ("IT",40) \
+  ]
+deptColumns = ["dept_name","dept_id"]
+deptDF = SPARK.createDataFrame(data=dept, schema = deptColumns)
+deptDF.printSchema()
+deptDF.show(truncate=False)
+
+# COMMAND ----------
+
+# join some tables
+notebook.display(empDF.join(deptDF,empDF.emp_dept_id ==  deptDF.dept_id,"fullouter"))
--- tests/test_local_databricks.py
+++ tests/test_local_databricks.py
@@ -4,3 +4,7 @@
 def test_import():
     import local_databricks
 
+def test_example():
+    import os
+    path = "tests/samples/note1.py"
+    os.system(f"python {path}")
